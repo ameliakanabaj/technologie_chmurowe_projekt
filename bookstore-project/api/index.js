391ed6428a21f7679b3641899850922aca800a0b
Amelia Kanabaj <a.kanabaj.124@studms.ug.edu.pl>
2025-05-25 12:15:00 +0200

    added communication between microservices from folder logic and api

--- bookstore-project/api/index.js
+++ bookstore-project/api/index.js
@@ -4,6 +4,29 @@ const PORT = 3001;
 
 app.use(express.json());
 
+app.post('/orders', async (req, res) => {
+  const { amount } = req.body;
+
+  try {
+    const fetchResponse = await fetch('http://logic:3002/calculate', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ amount }),
+    });
+
+    const result = await fetchResponse.json();
+
+    res.json({
+      message: 'Zamówienie przetworzone',
+      original: result.original,
+      final: result.final,
+    });
+  } catch (error) {
+    console.error('Błąd przy komunikacji z logic:', error.message);
+    res.status(500).json({ error: 'Coś poszło nie tak przy obliczeniach.' });
+  }
+});
+
 app.get('/', (req, res) => {
   res.send('API działa!');
 });
